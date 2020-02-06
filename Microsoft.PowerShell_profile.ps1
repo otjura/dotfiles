0427c3a39ad8b576b235a0824134d8671bc44c6e
Otso Rajala <otsor@outlook.com>
2020-02-07 01:08:56 +0200

    new powershell

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,27 @@
-function Prompt {
-  Write-Host(split-path($PWD) -Leaf) -nonewline -foregroundcolor Green
-  Write-Host("$") -nonewline -foregroundcolor Green
-  return " ";
+# usual bash lookalike prompt for powershell
+function prompt {
+	# current directory
+	$c = (Get-Location)
+
+	# convert home directory to tilde
+	$r = [regex]::Escape($HOME) + '(\\.*)*$'
+
+	# hostname
+	$h = $env:COMPUTERNAME.ToLower()
+
+	# get only current folder
+	#$p = Split-Path (Get-Location) -Leaf
+
+	#print prompt
+	"[$h`:$($c -replace $r, '~$1')$('>' * ($NestedPromptLevel + 1))] "
 }
+
+
+#############################################################
+#DEFAULT PROMPT for PowerShell 7.0 
+#https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_prompts?view=powershell-7
+#
+#function prompt {
+#	$(if (Test-Path variable:/PSDebugContext) { '[DBG]: ' }
+#		else { '' }) + 'PS ' + $(Get-Location) + $(if ($NestedPromptLevel -ge 1) { '>>' }) + '> '
+#}
